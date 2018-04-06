239c46f411e32dedeb1187064c41cdfb2dfc06a0
Macus Desir <macusdesir@gmail.com>
2018-04-06 18:39:54 -0400

    added faker gem, updated seed file, populated club database

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,5 @@
+class LandingController < ApplicationController
+  def index
+  	@club_images = Club.order("RANDOM()").limit(5).pluck(:photo)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+	Club.create({
+		name: Faker::Pokemon.name, city: Faker::Address.city, photo: Faker::Placeholdit.image
+		})
+end
+
+puts "hold up"
