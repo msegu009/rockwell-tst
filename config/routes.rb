39cfdd113fecef00791b8441f305e50c2cb0d672
Michael Segura <msegu009@outlook.com>
2018-04-06 19:20:14 -0400

    connected view with controller for signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'landing/index'
 
 
-  get 'city', to: 'city#city'
-
+  get "/city" => "city#city"
+  get "/signup" => "signup#signup"
+  # post "/signup" => "signup#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
