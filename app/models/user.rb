39a320b3ec1e498795267ea0937dc13085ed1b96
Michael Segura <msegu009@outlook.com>
2018-04-08 16:50:01 -0400

    fixed athens

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 	validates :email, uniqueness: true
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :save
 	validates :password_digest, presence: true
+	has_secure_password
 
 
 end
