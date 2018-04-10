cc23b6870321f7fea1cb52b52f2836de75be9fcc
Gabe <gwomble27@gmail.com>
2018-04-10 11:42:12 -0400

    Application.js fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require materialize
-//= require jquery
 //= require turbolinks
+//= require jquery
+//= require materialize
 //= require_tree .
--- app/assets/javascripts/city.js
+++ app/assets/javascripts/city.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
