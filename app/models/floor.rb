b1754de5f402782d65e2ba9891af45bc8447e890
Michael Segura <msegu009@outlook.com>
2018-04-13 16:12:26 -0400

    backend to have table availability reflect on reservations page

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -2,4 +2,8 @@ class Floor < ApplicationRecord
   belongs_to :club
   has_many :tables
   validates :club_id, presence: true
+
+  def available_tables(day)
+  	self.tables.reject { |t| t.bookings.where(day: day).count > 0 }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406212004) do
+ActiveRecord::Schema.define(version: 20180413194316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "day"
+    t.date "day"
     t.string "num_of_males"
     t.string "num_of_females"
     t.boolean "atmosphere"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20180406212004) do
     t.string "firstname"
     t.string "lastname"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "stripe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
